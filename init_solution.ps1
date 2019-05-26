69f6d45b06336ea7f6e7990778da3dda561f349c
Apollo3zehn <vwilms@outlook.com>
2019-05-26 15:47:30 +0200

    Download artifacts and build package.

---
+++ init_solution.ps1
@@ -0,0 +1,15 @@
+function Expand-WebArchive([string]$uri, [string]$runtime)
+{
+    $tmp = New-TemporaryFile
+    Invoke-WebRequest -Uri $uri -OutFile $tmp
+    $tmp | Expand-Archive -DestinationPath ./runtimes/$runtime/native
+    $tmp | Remove-Item
+}
+
+git submodule update --init --recursive --quiet
+
+$linuxUri   = "https://ci.appveyor.com/api/buildjobs/pb2k58en4cmt7txo/artifacts/artifacts.zip"
+$windowsUri = "https://ci.appveyor.com/api/buildjobs/x8ph2boakq3o3aj8/artifacts/artifacts.zip"
+
+Expand-WebArchive $linuxUri "linux-x64"
+Expand-WebArchive $windowsUri "win-x64"
