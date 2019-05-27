898d210c17da446fb820f8ee14bfbf5186f4c885
Vincent Wilms <vwilms@outlook.com>
2019-05-27 23:28:52 +0200

    Reenable tests.

--- init_solution.ps1
+++ init_solution.ps1
@@ -17,8 +17,8 @@ function Expand-WebArchiveWindows([string]$uri, [string]$runtime)
 
 git submodule update --init --recursive --quiet
 
-$linuxUri   = "https://ci.appveyor.com/api/buildjobs/tokk810oncanq905/artifacts/artifacts.zip"
-$windowsUri = "https://ci.appveyor.com/api/buildjobs/5am37tyjc74wjpgg/artifacts/artifacts.zip"
+$linuxUri   = "https://ci.appveyor.com/api/buildjobs/6wayul6wuod6oa5h/artifacts/artifacts.zip"
+$windowsUri = "https://ci.appveyor.com/api/buildjobs/wn4dljrhg1037hr6/artifacts/artifacts.zip"
 
 if ($isLinux)
 {
