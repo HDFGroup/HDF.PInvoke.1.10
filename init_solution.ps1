2f48c9a5c3e143a54a85b6641672183b4727d4f2
Vincent Wilms <vwilms@outlook.com>
2019-05-27 09:33:14 +0200

    Update links.

--- init_solution.ps1
+++ init_solution.ps1
@@ -8,8 +8,8 @@ function Expand-WebArchive([string]$uri, [string]$runtime)
 
 git submodule update --init --recursive --quiet
 
-$linuxUri   = "https://ci.appveyor.com/api/buildjobs/ip7977umdv2x48ei/artifacts/artifacts.zip"
-$windowsUri = "https://ci.appveyor.com/api/buildjobs/9o5k4y5svl8wea0o/artifacts/artifacts.zip"
+$linuxUri   = "https://ci.appveyor.com/api/buildjobs/yqyton27jvhrveyy/artifacts/artifacts.zip"
+$windowsUri = "https://ci.appveyor.com/api/buildjobs/39qb173mp0aa0u40/artifacts/artifacts.zip"
 
 Expand-WebArchive $linuxUri "linux-x64"
 Expand-WebArchive $windowsUri "win-x64"
