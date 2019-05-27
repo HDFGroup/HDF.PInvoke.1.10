7d04625dde6ea21c73a1c05939c8bf6d6d13760e
Vincent Wilms <vwilms@outlook.com>
2019-05-28 00:51:59 +0200

    Add win-x86.

--- init_solution.ps1
+++ init_solution.ps1
@@ -17,16 +17,19 @@ function Expand-WebArchiveWindows([string]$uri, [string]$runtime)
 
 git submodule update --init --recursive --quiet
 
-$linuxUri   = "https://ci.appveyor.com/api/buildjobs/6wayul6wuod6oa5h/artifacts/artifacts.zip"
-$windowsUri = "https://ci.appveyor.com/api/buildjobs/wn4dljrhg1037hr6/artifacts/artifacts.zip"
+$linuxUrl       = "https://ci.appveyor.com/api/buildjobs/rqfxh3nd3vxb44t1/artifacts/artifacts.zip"
+$windowsUrl_x64 = "https://ci.appveyor.com/api/buildjobs/375h5vbdbgo29xei/artifacts/artifacts.zip"
+$windowsUrl_x86 = "https://ci.appveyor.com/api/buildjobs/71yl34l1oha5sity/artifacts/artifacts.zip"
 
 if ($isLinux)
 {
-    Expand-WebArchiveLinux $linuxUri "linux-x64"
-    Expand-WebArchiveLinux $windowsUri "win-x64"
+    Expand-WebArchiveLinux $linuxUrl "linux-x64"
+    Expand-WebArchiveLinux $windowsUrl_x64 "win-x64"
+    Expand-WebArchiveLinux $windowsUrl_x86 "win-x86"
 }
 elseif ($isWindows)
 {
-    Expand-WebArchiveWindows $linuxUri "linux-x64"
-    Expand-WebArchiveWindows $windowsUri "win-x64"
+    Expand-WebArchiveWindows $linuxUrl "linux-x64"
+    Expand-WebArchiveWindows $windowsUrl_x64 "win-x64"
+    Expand-WebArchiveWindows $windowsUrl_x64 "win-x86"
 }
